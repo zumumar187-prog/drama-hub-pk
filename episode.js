bd1314dcc04a435f45489c341137da4439bbb841
zumumar187-prog <zumumar187@gmail.com>
2026-06-01 01:44:18 +0500

    Update episode.js

--- episode.js
+++ episode.js
@@ -25,8 +25,8 @@ if(episodes.length === 0){
 container.innerHTML = "<h2>No Episodes Found For This Drama</h2>";
 return;
 }
-document.getElementById("dramaTitle").innerText =
-"🎬 Kabhi Main Kabhi Tum";
+document.getElementById("dramaTitle").innerHTML =
+`🎬 Kabhi Main Kabhi Tum <span style="font-size:18px;color:#aaa;">(${episodes.length} Episodes)</span>`;
 episodes.forEach(ep => {
 
 container.innerHTML += `
